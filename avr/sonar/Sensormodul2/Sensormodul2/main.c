0a698e109b56ea1e734979f455da4ba14f532933
soderholmen <the-last-1@hotmail.com>
2017-11-12 11:51:21 -0500

    Sensormodul update

---
+++ avr/sonar/Sensormodul2/Sensormodul2/main.c
@@ -0,0 +1,194 @@
+//Frequency
+#define F_CPU 16000000
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <stdio.h>
+
+#include <util/delay.h>
+#include <stdlib.h>
+
+uint32_t sonar_data[4];
+uint32_t pulse = 0;
+unsigned short sonar_nr;
+
+#define blue_led_on() PORTC |= _BV(1);
+
+#define blue_led_off() PORTC &= ~_BV(1);
+//Mode
+int mode = 0;
+
+void sonar_timer_interrupt();
+
+void calc_sonar_data(unsigned short sonar_nr, uint32_t pulse);
+
+int main(void)
+{
+	//Pins is now an output and input
+	DDRA = 0b00000010;
+	DDRB = 0b00000010;
+	DDRC = 0b00000010;
+	DDRD = 0b00000010;
+	_delay_ms(10);
+	
+	
+	//Turn on interrupt on PCMSK2 pins
+	PCICR |= _BV(3)| _BV(2)| _BV(1)| _BV(0);
+	//Theses pins now trigger an interrupt
+	PCMSK0 |= _BV(0);
+	PCMSK1 |= _BV(0);
+	PCMSK2 |= _BV(0);
+	PCMSK3 |= _BV(0);
+
+	TCCR1B = 0;
+	//Turn on global interrupt
+	sei();
+	
+	
+	while(1 == 1)
+	{
+		_delay_ms(100);
+		switch (mode)
+		{
+			case 0:
+				//Triggerpin is high for 15uS
+				PORTA|=_BV(1);
+				_delay_us(15);
+				PORTA &= ~_BV(1);
+				mode = 5;
+				_delay_ms(10);
+				break;
+			case 1:
+				//Triggerpin is high for 15uS
+				PORTB|=_BV(1);
+				_delay_us(15);
+				PORTB &= ~_BV(1);
+				mode = 5;
+				_delay_ms(10);
+				break;
+			case 2:
+				//Triggerpin is high for 15uS
+				PORTC|=_BV(1);
+				_delay_us(15);
+				PORTC &= ~_BV(1);
+				mode = 5;
+				_delay_ms(10);
+				break;
+			case 3:
+				//Triggerpin is high for 15uS
+				PORTD|=_BV(1);
+				_delay_us(15);
+				PORTD &= ~_BV(1);
+				mode = 5;
+				_delay_ms(10);
+				break;
+		
+		
+		
+		}
+	}
+}
+
+
+void sonar_timer_interrupt(int sonar_nr){
+	
+	switch (sonar_nr)
+	{
+		case 0:
+		//LOW -> HIGH
+		if( (PINA & (1 << PINA0)) == 1)
+		{
+			// Raknare=0
+			TCNT1=0;
+			TCCR1B = (0 << CS12) | (0 << CS11) | (1 << CS10);
+		}
+		//HIGh -> LOW
+		else{
+			//Stops conter
+			TCCR1B = (0 << CS12) | (0 << CS11) | (0 << CS10);
+			pulse=TCNT1;
+			calc_sonar_data(0, pulse);
+			mode = 1;
+			
+		}break;
+		case 1:
+		//LOW -> HIGH
+		if( (PINB & (1 << PINB0)) == 1)
+		{
+			// Raknare=0
+			TCNT1=0;
+			TCCR1B = (0 << CS12) | (0 << CS11) | (1 << CS10);
+		}
+		//HIGh -> LOW
+		else{
+			//Stops conter
+			TCCR1B = (0 << CS12) | (0 << CS11) | (0 << CS10);
+			pulse=TCNT1;
+			calc_sonar_data(0, pulse);
+			mode = 2;
+		}break;
+		case 2:
+		//LOW -> HIGH
+		if( (PINC & (1 << PINC0)) == 1)
+		{
+			// Raknare=0
+			TCNT1=0;
+			TCCR1B = (0 << CS12) | (0 << CS11) | (1 << CS10);
+		}
+		//HIGh -> LOW
+		else{
+			//Stops conter
+			TCCR1B = (0 << CS12) | (0 << CS11) | (0 << CS10);
+			pulse=TCNT1;
+			calc_sonar_data(0, pulse);
+			mode = 3;
+		}break;
+		case 3:
+		//LOW -> HIGH
+		if( (PIND & (1 << PIND0)) == 1)
+		{
+			// Raknare=0
+			TCNT1=0;
+			TCCR1B = (0 << CS12) | (0 << CS11) | (1 << CS10);
+		}
+		//HIGh -> LOW
+		else{
+			//Stops conter
+			TCCR1B = (0 << CS12) | (0 << CS11) | (0 << CS10);
+			pulse=TCNT1;
+			calc_sonar_data(0, pulse);
+			mode = 0;
+		}break;
+	}
+}
+
+void calc_sonar_data(unsigned short sonar_nr, uint32_t pulse){
+	uint32_t cm;
+	cm = (pulse/480)+2;
+	sonar_data[sonar_nr] = cm;
+}
+
+
+ISR(PCINT0_vect){sonar_timer_interrupt(0);}
+
+
+ISR(PCINT1_vect){sonar_timer_interrupt(1);}
+
+
+ISR(PCINT2_vect){sonar_timer_interrupt(2);}
+
+
+ISR(PCINT3_vect){sonar_timer_interrupt(3);}
+
+
+
+//Take too long time too get data, reset
+ISR (TIMER1_OVF_vect)
+{
+	//Stops counter
+	TCCR1B=0;
+	//////////////////////////////////////////
+	//Reset everything
+	//////////////////////////////////////////
+}
+
