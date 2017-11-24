80481846175a638a912d23ba4515f6c99528cd7f
Svennas <gustavsvennas@gmail.com>
2017-11-24 14:40:33 +0100

    Hall effect works, commented and restructured motormodul_v1

--- avr/motormodul/motormodul_v1/motormodul_v1/motormodul_v1.c
+++ avr/motormodul/motormodul_v1/motormodul_v1/motormodul_v1.c
@@ -3,7 +3,16 @@
  *
  * Created: 11/12/2017 4:28:20 PM
  *  Author: marli763, krisi211, gussö811
- */ 
+ */
+
+
+
+/* --- TODO TODO TODO TODO TODO TODO ---
+ * Fix data types when calculating RPM/RPS
+ * Comment properly
+ * Remove lcd compability (?)
+ *
+*/
 
 #define F_CPU 16000000
 
@@ -23,18 +32,18 @@
 #define timer_led_is_on() bit_is_set(PORTA,2)
 
 //		--- PWM ---
-const int natural = 3000; //Pulse is 1,5 ms
-const int max_left = 3900; //Ish max right (pulse should be 2 ms)
-const int max_right = 2100; //Ish max left (pulse should be 1ms)
-const int max_speed; //FIND ITS VALUE
-const int min_speed = 3170; //FIND ITS VALUE
-int turn; //Turn < natural => right, turn > natural => left
+const int natural = 3000;		//Pulse is 1,5 ms
+const int max_left = 3900;		//Ish max right (pulse should be 2 ms)
+const int max_right = 2100;		//Ish max left (pulse should be 1ms)
+const int max_speed;			//FIND ITS VALUE
+const int min_speed = 3170;		//FIND ITS VALUE
+int turn;						//Turn < natural => right, turn > natural => left
 int scale_speed;
 
 //		--- HALLEFFECT ---
-volatile long ticks_elapsed;
-volatile float time_elapsed;
-volatile float rpm;
+volatile long ticks_elapsed;	//ticks since last timer-intr
+volatile float time_elapsed;	//ticks converted into time
+volatile float rpm;				//revolutions per minute
 volatile int tot_overflow;
 uint32_t current_ticks;
 volatile long TIMER_TICKS = 65536;
@@ -46,16 +55,18 @@ motormodul_PA_data data_in;
 //data sent from 'motormodul' (A = AVR) to rasberry pi (=P)
 motormodul_AP_data data_out;
 
+//      --- P algorithm ---
+double currVal ;
 
 
-//Timer3 initiate for Hall effect
+
+
 void timer3_init()
 {
+	//		--- TIMER FOR HALL EFFECT SENSOR SETUP ---
 	TCCR3A |= _BV(WGM32);
-	TCCR3B |= _BV(CS32);//16MHz, 16 bit timer and 256 prescaler gives 0.95367431640625Hz
-	
-	TCNT3 = 0;
-	
+	TCCR3B |= _BV(CS32);		//16MHz, 16 bit timer and 256 prescaler gives 0.95367431640625Hz
+	TCNT3 = 0;					// Reset counter
 	TIMSK3 |= (1 << TOIE3);
 }
 
@@ -63,8 +74,8 @@ void pwm_init()
 {
 	//		--- PWM SETUP ---
 	//Pin set-up
-	DDRD |= _BV(5); //Pinne 19, styrservot
-	DDRD |= _BV(4); //Pinne 18, motorn
+	DDRD |= _BV(5);				//Pinne 19, styrservot
+	DDRD |= _BV(4);				//Pinne 18, motorn
 	//timer set-up
 	TCCR1A |= _BV(1) | _BV(7) | _BV(5);
 	TCCR1B |= _BV(3)| _BV(4) | _BV(1);
@@ -75,7 +86,7 @@ void pwm_init()
 	OCR1A = turn;
 	OCR1B = scale_speed;
 	 led_on();
-	_delay_ms(5000);
+	_delay_ms(5000);			// delay so that the motor control receives it's neutral properly
 	 led_off();
 }
 
@@ -84,8 +95,8 @@ void halleffect_init()
 	//		--- HALL EFFECT SETUP ---
 	// PINCHANGE INTERRUPT FOR MEASURING PULSES
 	DDRA  = 0b11111110;
-	PCICR |= (1 << PCIE0);    // set PCIE0 to enable PCMSK0 scan
-	PCMSK0 |= (1 << PCINT0);  // set PCINT0 to trigger an interrupt on state change
+	PCICR |= (1 << PCIE0);		// set PCIE0 to enable PCMSK0 scan
+	PCMSK0 |= (1 << PCINT0);	// set PCINT0 to trigger an interrupt on state change
 	tot_overflow = 0;
 }
 void lcd_init(){
@@ -108,8 +119,8 @@ void scale(){
 	{
 		turn = turn;
 	}
-	//Uppdatera granser för hastighet,
-	//maxhastighet troligen innan speed = 40000
+	//Update speed limits,
+	//Probable max speed = 40000
 	if(data_in.speed <= 200 && data_in.speed >= 101)
 	{
 		scale_speed = (int)(3180 + (data_in.speed - 100));
@@ -127,15 +138,16 @@ void scale(){
 
 int main(void)
 {
+	//		--- Initialize timers & 'modules' ---
 	//lcd_init();
 	pwm_init();
 	halleffect_init();
 	spi_init();
-	// TIMER INTERRUPT FOR MEASURING SPEED
 	timer3_init();
 
 	sei();
 	
+	//		--- Main loop, receive tasks from master ---
     while(1)
     {
 		data_out.curr_rpm = rpm;
@@ -152,27 +164,33 @@ int main(void)
 
 ISR(PCINT0_vect)
 {
+	//		--- Pin change interrupt for hall effect sensor ---
+	// Just messure on incomming magnet, not outgoing
 	if( PINA & ((1 << PIND0) == 1)){
-
+		
+		// Debug LED
 		if (led_is_on())
 			led_off();
 		else
 			led_on();
 	
+		// RPM Calculations TODO: CHANGE DATA TYPES
 		current_ticks = TCNT3;
 		ticks_elapsed = (tot_overflow * TIMER_TICKS) + current_ticks;
 		time_elapsed = (float) ticks_elapsed * seconds_per_tick;//seconds
-	
-		tot_overflow = 0;
+		tot_overflow = 0;		//Reset registered overflows and timer counter
 		TCNT3 = 0;
 	
-		rpm = (float) (1/(time_elapsed*2))*60 ;
+		rpm = (float) (1/(time_elapsed*4))*60 ;
 	}
 }
 
 
 ISR(TIMER3_OVF_vect)
 {
+	//		--- Timer interupt (0.953~ Hz) ---
+	
+	// Debug LED (Not connected to board currently 24/11)
 	if (timer_led_is_on())
 	timer_led_off();
 	else
@@ -182,5 +200,6 @@ ISR(TIMER3_OVF_vect)
 	if(tot_overflow >= 2){
 		rpm = 0;
 		new_rpm=1;
+		
 	}
 }
