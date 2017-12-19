98c17b2b73920e0d5e39c040995940ad6de43fa0
husselito <martin.lindkvist96@gmail.com>
2017-12-19 14:10:59 +0100

    Tog bort TODO från motormodul_v1

--- avr/motormodul/motormodul_v1/motormodul_v1/motormodul_v1.c
+++ avr/motormodul/motormodul_v1/motormodul_v1/motormodul_v1.c
@@ -7,15 +7,6 @@
 
 
 
-/* --- TODO TODO TODO TODO TODO TODO ---
- * Fix data types when calculating RPM/RPS
- * Comment properly
- * Merge with master
- * Incorporate RPM into scale
- * Remove lcd compability (?)
- *
-*/
-
 #define F_CPU 16000000
 
 #include <avr/io.h>
